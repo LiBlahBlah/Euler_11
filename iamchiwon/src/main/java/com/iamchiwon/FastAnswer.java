2014aa4c58367bd009c607987c0fdf911688b69b
Song Chiwon <iamchiwon@gmail.com>
2017-06-09 01:20:37 +0900

    좀이라도 더 빨리 해보자.. 2~3 밀리세컨드 정도?

--- iamchiwon/src/main/java/com/iamchiwon/FastAnswer.java
+++ iamchiwon/src/main/java/com/iamchiwon/FastAnswer.java
@@ -11,8 +11,7 @@ public class FastAnswer {
         numSource()
                 .doOnSubscribe(dontcare -> start = System.nanoTime())
                 .doOnTerminate(() -> System.out.println("TIME : " + (System.nanoTime() - start) + " ns"))
-                .filter(val -> val != 0)
-                .reduce((n1, n2) -> Math.max(n1, n2))
+                .reduce((n1, n2) -> n1 > n2 ? n1 : n2)
                 .subscribe(max -> {
                     System.out.println("MAX : " + max);
                 });
@@ -20,31 +19,23 @@ public class FastAnswer {
 
     private static Observable<Long> numSource() {
         return Observable.create(emitter -> {
-            //수평으로 연속된 숫자들
-            for (int y = 0; y < 20; y++) {
-                for (int x = 0; x < 17; x++) {
-                    emitter.onNext(horizental(x, y));
-                }
-            }
 
-            //수직으로 연속된 숫자들
-            for (int x = 0; x < 20; x++) {
-                for (int y = 0; y < 17; y++) {
-                    emitter.onNext(vertical(x, y));
+            for (int b = 0; b < 17; b++) {
+                for (int a = 0; a < 20; a++) {
+                    //수평으로 연속된 숫자들
+                    emitter.onNext(horizental(b, a));
+                    //수직으로 연속된 숫자들
+                    emitter.onNext(vertical(a, b));
                 }
-            }
 
-            //오른쪽 대각선으로 연속된 숫자들 (백슬래시)
-            for (int x = 0; x < 17; x++) {
-                for (int y = 0; y < 17; y++) {
-                    emitter.onNext(backslash(x, y));
+                //오른쪽 대각선으로 연속된 숫자들 (백슬래시)
+                for (int a = 0; a < 17; a++) {
+                    emitter.onNext(backslash(a, b));
                 }
-            }
 
-            //왼쪽 대각선으로 연속된 숫자들 (슬래시)
-            for (int x = 3; x < 20; x++) {
-                for (int y = 0; y < 17; y++) {
-                    emitter.onNext(slash(x, y));
+                //왼쪽 대각선으로 연속된 숫자들 (슬래시)
+                for (int a = 3; a < 20; a++) {
+                    emitter.onNext(slash(a, b));
                 }
             }
 
@@ -53,34 +44,38 @@ public class FastAnswer {
     }
 
     private static long horizental(int x, int y) {
-        long value = Data.getNum(x, y);
-        value *= Data.getNum(x + 1, y);
-        value *= Data.getNum(x + 2, y);
-        value *= Data.getNum(x + 3, y);
+        int position = y * 20 + x;
+        long value = Data.numberTable[position];
+        value *= Data.numberTable[position + 1];
+        value *= Data.numberTable[position + 2];
+        value *= Data.numberTable[position + 3];
         return value;
     }
 
     private static long vertical(int x, int y) {
-        long value = Data.getNum(x, y);
-        value *= Data.getNum(x, y + 1);
-        value *= Data.getNum(x, y + 2);
-        value *= Data.getNum(x, y + 3);
+        int position = y * 20 + x;
+        long value = Data.numberTable[position];
+        value *= Data.numberTable[position + 20];
+        value *= Data.numberTable[position + 40];
+        value *= Data.numberTable[position + 60];
         return value;
     }
 
     private static long backslash(int x, int y) {
-        long value = Data.getNum(x, y);
-        value *= Data.getNum(x + 1, y + 1);
-        value *= Data.getNum(x + 2, y + 2);
-        value *= Data.getNum(x + 3, y + 3);
+        int position = y * 20 + x;
+        long value = Data.numberTable[position];
+        value *= Data.numberTable[position + 20 + 1];
+        value *= Data.numberTable[position + 40 + 2];
+        value *= Data.numberTable[position + 60 + 3];
         return value;
     }
 
     private static long slash(int x, int y) {
-        long value = Data.getNum(x, y);
-        value *= Data.getNum(x - 1, y + 1);
-        value *= Data.getNum(x - 2, y + 2);
-        value *= Data.getNum(x - 3, y + 3);
+        int position = y * 20 + x;
+        long value = Data.numberTable[position];
+        value *= Data.numberTable[position + 20 - 1];
+        value *= Data.numberTable[position + 40 - 2];
+        value *= Data.numberTable[position + 60 - 3];
         return value;
     }
 }
